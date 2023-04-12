4855e6ab9142276507f6e1adb6d38d2b85a0bb97
hmkimm <lisamin7@naver.com>
2023-04-13 01:48:25 +0900

    add delete btn

--- list.js
+++ list.js
@@ -3,13 +3,26 @@ const $inp = document.querySelector(".text");
 const $listWrap = document.querySelector(".list-wrap");
 const $delete = document.querySelector(".delete");
 
+// 할일 추가 버튼
 $add.addEventListener("click", () => {
+  if ($inp.value == "") {
+    alert("글자를 입력하세요!");
+  }
   const $myLi = document.createElement("ul");
-  const $myBtn = document.createElement("button");
+  // const $myBtn = document.createElement("button");
   $myLi.innerHTML = $inp.value;
-  $myBtn.innerHTML = "+";
+  // $myBtn.innerHTML = "+";
   $listWrap.appendChild($myLi);
-  $listWrap.appendChild($myBtn);
+  // $listWrap.appendChild($myBtn);
+  $inp.value = "";
 
   $myLi.style.backgroundColor = "white";
+});
+
+// 모두 삭제 버튼
+$delete.addEventListener("click", () => {
+  $myLi.innerHTML = "";
+  $listWrap.removeChild("ul");
+  $myLi.remove();
+  const $remove = (document.querySelector("ul").innerHTML = "");
 });
