d80edd604f37070dbc5eef2dfdfdd32be1eec328
hmkimm <lisamin7@naver.com>
2023-04-14 04:26:46 +0900

    add delete, complete functions

--- list.js
+++ list.js
@@ -1,51 +1,58 @@
-const $add = document.querySelector(".add");
-const $inp = document.querySelector(".text");
-const $listWrap = document.querySelector(".list-wrap");
+const add = document.querySelector(".add");
+const inp = document.querySelector(".text");
+const listWrap = document.querySelector(".list-wrap");
 const $delete = document.querySelector(".delete");
 
-// 할일 추가 버튼
-$add.addEventListener("click", () => {
-  if ($inp.value == "") {
-    alert("글자를 입력하세요!");
+// 할일 추가, 완료, 삭제 버튼
+add.addEventListener("click", () => {
+  if (inp.value == "") {
+    alert("할 일을 입력하세요!");
   } else {
-    const $myLi = document.createElement("li");
-    const $myBtn = document.createElement("button");
+    const myLi = document.createElement("li");
+    const myBtn = document.createElement("button");
 
-    $myLi.innerHTML = $inp.value;
-    $myBtn.innerHTML = "X";
-    $myBtn.setAttribute("type", "button");
+    myLi.innerHTML = inp.value;
+    myBtn.innerHTML = "X";
+    myBtn.setAttribute("type", "button");
 
-    $listWrap.appendChild($myLi);
-    $myLi.appendChild($myBtn);
+    listWrap.appendChild(myLi);
+    myLi.appendChild(myBtn);
 
-    $myLi.classList.add("li-style");
-    $inp.value = "";
+    myLi.classList.add("li-style");
+    inp.value = "";
+
+    myLi.addEventListener("click", () => {
+      myLi.classList.toggle("horizontal-line");
+    });
+    myBtn.addEventListener("click", () => {
+      listWrap.removeChild(myLi);
+    });
   }
 });
 
 // 개별삭제 버튼
-const $delBtn = document.querySelectorAll("li button");
-console.log($delBtn.length);
-for (let i = 0; i < $delBtn.length; i++)
-  $delBtn[i].addEventListener("click", () => {
-    document.querySelector(".list-wrap").removeChild("$myLi");
-    console.log($delBtn.length);
-  });
+// const delBtn = document.querySelectorAll("li button");
+// console.log(delBtn.length);
+// for (let i = 0; i < delBtn.length; i++)
+//   delBtn[i].addEventListener("click", () => {
+//     document.querySelector(".list-wrap").removeChild("myLi");
+//     console.log(delBtn.length);
+//   });
 
-// $myBtn.onClick = function (e) {
+// myBtn.onClick = function (e) {
 //   let pnode = e.target.parentNode;
-//   $listWrap.removeChild(pnode);
+//   listWrap.removeChild(pnode);
 // };
 
 // // 완료 처리 버튼
-// $myLi.addEventListener("click", () => {
-//   $myLi.classList.add("horizontal-line");
+// myLi.addEventListener("click", () => {
+//   myLi.classList.add("horizontal-line");
 // });
 
 // 모두 삭제 버튼
 $delete.addEventListener("click", () => {
   const $lis = document.querySelectorAll("li");
   for (let i = 0; i < $lis.length; i++) {
-    $listWrap.removeChild($lis[i]);
+    listWrap.removeChild($lis[i]);
   }
 });
